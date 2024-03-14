e8a1f0159cc66553286abfd75d2f686dd3ff9de9
Tetrokun <philip.pasquale@gmail.com>
2024-03-14 17:33:14 -0400

    Create TOHO_config.py

---
+++ configs/TOHO_config.py
@@ -0,0 +1,10 @@
+
+
+class configimport:
+    def __init__(self):
+        self.plex_baseurl = 'http://Plex Server:32400'
+        self.plex_token = 'Plex Token'
+        self.db_path = 'touhou-music.db'
+        print("config loaded")
+        print()
+        
